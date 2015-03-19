2cf8f3dfdf55dc46716f2e9684c0908af997aaa1
carlosthe19916 <carlosthe19916@gmail.com>
2015-03-19 16:37:17 -0500

    1

--- producto-testsuite/producto-testsuite-model/src/test/java/org/sistcoop/producto/models/ProductoProviderTest.java
+++ producto-testsuite/producto-testsuite-model/src/test/java/org/sistcoop/producto/models/ProductoProviderTest.java
@@ -7,6 +7,7 @@ import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
 
 import java.io.File;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -21,70 +22,54 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.sistcoop.producto.models.enums.TipoPersona;
 import org.sistcoop.producto.models.jpa.JpaProductoProvider;
-import org.sistcoop.producto.models.jpa.ProductoCreditoAdapter;
-import org.sistcoop.producto.models.utils.ModelToRepresentation;
-import org.sistcoop.producto.models.utils.RepresentationToModel;
-import org.sistcoop.producto.representations.idm.ProductoCreditoRepresentation;
+import org.sistcoop.producto.models.jpa.ProductoAdapter;
+import org.sistcoop.producto.models.jpa.entities.ProductoEntity;
+import org.sistcoop.producto.provider.Provider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.sun.net.ssl.internal.ssl.Provider;
-
 @RunWith(Arquillian.class)
 @UsingDataSet("empty.xml")
-public class ProductoCreditoProviderTest {
+public class ProductoProviderTest {
 
-	Logger log = LoggerFactory.getLogger(ProductoCreditoProviderTest.class);
+	Logger log = LoggerFactory.getLogger(ProductoProviderTest.class);
 
-	@Inject
-	private ProductoProvider productoProvider;
+	//@Inject
+	//private TipoDocumentoProvider tipoDocumentoProvider;
 
 	@Deployment
 	public static WebArchive createDeployment() {
-		File[] dependencies = Maven.resolver().resolve("org.slf4j:slf4j-simple:1.7.10")
-				.withoutTransitivity().asFile();
+		File[] dependencies = Maven.resolver()
+				.resolve("org.slf4j:slf4j-simple:1.7.10")
+				.withoutTransitivity()
+				.asFile();
 
 		WebArchive war = ShrinkWrap.create(WebArchive.class, "test.war")
-		/** persona-core */
-		.addPackage(ProductoCreditoRepresentation.class.getPackage())
-
-		/** persona-core-jaxrs */
-		//.addPackage(JaxRsActivator.class.getPackage())
-
-		/** persona-client */
-		//.addClasses(TipoDocumentoResource.class, TiposDocumentoResource.class)
-
-		/** persona-services */
-		//.addClasses(TipoDocumentoResourceImpl.class, TiposDocumentoResourceImpl.class)
-
-		/** persona-model-api */
+		/** persona-model-api **/
 		.addClass(Provider.class)
 		.addClass(ProductoProvider.class)
-		.addPackage(ProductoCreditoModel.class.getPackage())
+
+		.addPackage(ProductoModel.class.getPackage())
 		.addPackage(TipoPersona.class.getPackage())
 
-		.addClass(RepresentationToModel.class)
-		.addClass(ModelToRepresentation.class)
-
-		/** persona-model-jpa */
-		.addClass(ProductoCreditoAdapter.class)
+		/** persona-model-jpa **/
 		.addClass(JpaProductoProvider.class)
+		.addPackage(ProductoAdapter.class.getPackage())
+
+		.addPackage(ProductoEntity.class.getPackage())
 
 		.addAsResource("META-INF/test-persistence.xml", "META-INF/persistence.xml")
 		.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml")
 		.addAsWebInfResource("test-ds.xml");
 
 		war.addAsLibraries(dependencies);
-		
+
 		return war;
 	}
-	
+
 	@Test
-	public void prueba(){
-		String a = "car";
-		String b = "los";
-		String c = "carlos";
+	public void addTipoDocumento() {
 		
-		assertThat(a+b, is(equalTo(c)));	
 	}
+	
 }
